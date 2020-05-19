0cbc266ce0f930258aefe5ac3229b3f29975c01c
Dexter <61207138+NubDexter@users.noreply.github.com>
2020-05-19 18:13:22 +0530

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,19 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.**
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("`Abey Zinda hun!` **ψ(｀∇´)ψ**\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
+                     # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
+                     "`Bot created by:` [Dexter](tg://user?id=440510599),\n"
+                     f"`Sabka Baap`: {DEFAULTUSER}\n\n"
+                     "https://github.com/NubDexter/DexGram")
